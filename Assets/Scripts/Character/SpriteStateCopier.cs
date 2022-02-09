0b8d57565e7dbfbf4587fdc4e124b45637110bac
davidchermanto <davidchermanto@gmail.com>
2022-02-10 04:32:13 +1100

    2D Sprite Shadow Shader

---
+++ Assets/Scripts/Character/SpriteStateCopier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteStateCopier : MonoBehaviour
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private SpriteRenderer targetRenderer;
+
+    [SerializeField] private Vector3 posOffset;
+    [SerializeField] private Quaternion rotOffset;
+
+    private SpriteRenderer ownRenderer;
+
+    private void Start()
+    {
+        ownRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ownRenderer.sprite = targetRenderer.sprite;
+        transform.position = targetTransform.position + posOffset;
+        transform.rotation = Quaternion.Euler(targetTransform.eulerAngles.x + rotOffset.x,
+            targetTransform.eulerAngles.y + rotOffset.y, targetTransform.eulerAngles.z + rotOffset.z);
+    }
+}
